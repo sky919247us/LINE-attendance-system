ad18b823e524c4aacc17be6514be718ac507e8ef
sky919247us <sky919247us@gmail.com>
2025-10-06 13:41:21 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js
+
+const API_CONFIG = {
+  // 正式環境的 API URL
+  apiUrl: "https://script.google.com/macros/s/AKfycbzvZPwRXNBfbsi9YICkDCu3lfTRjofMth_OwViQzGVGkoba-2yunqlHQluEp3QF_qzh/exec",
+  // 新增回呼網址
+  redirectUrl: "https://0rigind1865-bit.github.io/Attendance-System/"
+  // 你也可以在這裡加入其他設定，例如：
+  // timeout: 5000,
+  // version: 'v1.0'
+};
